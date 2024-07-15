53eb799ee3bb32d4a6ca7c153bc801fcd33eab18
0-BSCode <bryanipsanchez@gmail.com>
2024-07-15 22:24:01 +0800

    Break down shopping_list_catalog widget

--- lib/feature/shopping_list/presentations/component/shopping_list_catalog.dart
+++ lib/feature/shopping_list/presentations/component/shopping_list_catalog.dart
@@ -1,5 +1,6 @@
 import 'package:cooki/common/theme/app_colors.dart';
 import 'package:cooki/common/theme/app_text_styles.dart';
+import 'package:cooki/feature/shopping_list/data/model/output/shopping_list_output.dart';
 import 'package:cooki/feature/shopping_list/presentations/view_model/shopping_list_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -15,62 +16,90 @@ class ShoppingListCatalog extends StatelessWidget {
       return SingleChildScrollView(
         child: Column(
           children: [
-            for (var list in state.shoppingLists)
-              Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      color: AppColors.accent,
-                    ),
-                    width: double.infinity,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 24.0, horizontal: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              IconButton(
-                                onPressed: () =>
-                                    context.read<ShoppingListViewModel>().add(
-                                          ShoppingListDeleted(list.id),
-                                        ),
-                                icon: const Icon(
-                                  Icons.delete,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 8,
-                              ),
-                              Text(
-                                list.name,
-                                style: AppTextStyles.titleMedium.copyWith(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Text(
-                            '${list.items.length.toString()} items',
-                            style: AppTextStyles.bodyMedium.copyWith(
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                ],
-              ),
+            for (var list in state.shoppingLists) ShoppingListCard(list: list),
           ],
         ),
       );
     });
+  }
+}
+
+class ShoppingListCard extends StatelessWidget {
+  const ShoppingListCard({
+    super.key,
+    required this.list,
+  });
+
+  final ShoppingListOutput list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            color: AppColors.accent,
+          ),
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 24.0,
+              horizontal: 16.0,
+            ),
+            child: ShoppingListCardContent(list: list),
+          ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+      ],
+    );
+  }
+}
+
+class ShoppingListCardContent extends StatelessWidget {
+  const ShoppingListCardContent({
+    super.key,
+    required this.list,
+  });
+
+  final ShoppingListOutput list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            IconButton(
+              onPressed: () => context.read<ShoppingListViewModel>().add(
+                    ShoppingListDeleted(list.id),
+                  ),
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              list.name,
+              style: AppTextStyles.titleMedium.copyWith(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        Text(
+          '${list.items.length.toString()} items',
+          style: AppTextStyles.bodyMedium.copyWith(
+            color: Colors.white,
+          ),
+        )
+      ],
+    );
   }
 }
