23287e4e9ef1fcf378c9e963ac7f089b708acc08
0-BSCode <bryanipsanchez@gmail.com>
2024-07-25 01:14:02 +0800

    Edit styles of shopping list screens

--- lib/feature/shopping_list/presentations/screen/shopping_list_screen.dart
+++ lib/feature/shopping_list/presentations/screen/shopping_list_screen.dart
@@ -3,6 +3,9 @@ import 'package:cooki/common/component/main_scaffold.dart';
 import 'package:cooki/common/extension/enum_extension.dart';
 import 'package:cooki/common/hook/use_on_widget_load.dart';
 import 'package:cooki/common/navigation/app_routes.dart';
+import 'package:cooki/common/theme/app_colors.dart';
+import 'package:cooki/common/theme/app_text_styles.dart';
+import 'package:cooki/common/theme/app_theme_data.dart';
 import 'package:cooki/feature/preferences/data/enum/product_category.dart';
 import 'package:cooki/feature/shopping_list/data/di/shopping_list_service_locator.dart';
 import 'package:cooki/feature/shopping_list/data/model/input/update_shopping_list_item_input.dart';
@@ -76,46 +79,68 @@ class ShoppingListScreen extends HookWidget {
         ),
       ],
       contentPadding: EdgeInsets.zero,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                TextButton(
-                  child: Text(
-                    'Go Back',
-                  ),
-                  onPressed: () {
-                    context
-                        .read<ShoppingListViewModel>()
-                        .add(const ShoppingListsRequested());
-
-                    context.go(
-                      Uri(
-                        path: AppRoutes.shoppingLists,
-                      ).toString(),
-                    );
-                  },
+      body: SizedBox(
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsetsDirectional.symmetric(
+            vertical: 12,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  for (var category in itemsByCategory.keys)
+                    _ShoppingListCategory(
+                      id: shoppingList.id,
+                      category: category,
+                      items: itemsByCategory[category] ?? [],
+                    ),
+                  Container(
+                    width: double.infinity,
+                    color: AppColors.backgroundSecondary,
+                    padding: EdgeInsets.all(
+                      12,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(children: [
+                          Icon(
+                            Icons.monetization_on,
+                            size: 24,
+                          ),
+                          const SizedBox(width: 12),
+                          Text("Total"),
+                        ]),
+                        Text(
+                          'Php ${shoppingList.budget.toStringAsFixed(2)}',
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              IconButton(
+                color: AppColors.accent,
+                style: IconButton.styleFrom(
+                  backgroundColor: AppColors.backgroundSecondary,
                 ),
-                for (var category in itemsByCategory.keys)
-                  _ShoppingListCategory(
-                    id: shoppingList.id,
-                    category: category,
-                    items: itemsByCategory[category] ?? [],
-                  ),
-              ],
-            ),
-            TextButton(
-              onPressed: () => context.go(
-                Uri(
-                  path: '${AppRoutes.shoppingLists}/$id/create-item',
-                ).toString(),
+                padding: const EdgeInsets.all(
+                  16.0,
+                ),
+                onPressed: () => context.go(
+                  Uri(
+                    path: '${AppRoutes.shoppingLists}/$id/create-item',
+                  ).toString(),
+                ),
+                icon: Icon(
+                  Icons.add,
+                  size: 24.0,
+                ),
               ),
-              child: Text(
-                'Create Item',
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -139,8 +164,31 @@ class _ShoppingListCategory extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          category.apiValue,
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 12,
+          ),
+          decoration: BoxDecoration(
+            color: AppColors.accent,
+          ),
+          child: Row(
+            children: [
+              SizedBox(
+                height: 24,
+                width: 24,
+                child: category.icon,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Text(
+                category.apiValue,
+                style: AppTextStyles.titleSmall,
+              ),
+            ],
+          ),
         ),
         for (var item in items)
           _ShoppingListItem(
@@ -162,34 +210,45 @@ class _ShoppingListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Checkbox(
-          value: item.isChecked,
-          onChanged: (res) {
-            context.read<ShoppingListViewModel>().add(
-                  ShoppingListItemToggled(
-                    item,
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 8,
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: item.isChecked,
+                onChanged: (res) {
+                  context.read<ShoppingListViewModel>().add(
+                        ShoppingListItemToggled(
+                          item,
+                        ),
+                      );
+                },
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        item.label,
+                        style: AppTextStyles.bodyLarge,
+                      ),
+                      const SizedBox(width: 12),
+                      Text(
+                        '(${item.quantity.toString()} x ${item.product.unitSize})',
+                      ),
+                    ],
                   ),
-                );
-          },
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text(
-                  item.label,
-                ),
-                const SizedBox(width: 12),
-                Text(
-                  '(${item.quantity.toString()} x ${item.product.unitSize})',
-                ),
-              ],
-            ),
-            Text(
-              'Php ${item.product.price.toString()}',
-            ),
-          ],
+                  Text(
+                    'Php ${item.product.price.toString()}',
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
         const Spacer(),
         IconButton(
