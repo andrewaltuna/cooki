57fe36d0a66d76b71c9292b94ff4183cefed26eb
0-BSCode <bryanipsanchez@gmail.com>
2024-07-24 22:16:55 +0800

    Created receivers for API responses

---
+++ lib/feature/shopping_list/data/model/shopping_list.dart
@@ -0,0 +1,64 @@
+import 'package:cooki/feature/shopping_list/data/model/output/shopping_list_item_output.dart';
+import 'package:equatable/equatable.dart';
+
+class ShoppingList extends Equatable {
+  const ShoppingList({
+    required this.id,
+    required this.name,
+    required this.budget,
+    required this.items,
+    required this.userId,
+  });
+
+  static const ShoppingList empty = ShoppingList(
+    id: '',
+    userId: '',
+    name: '',
+    budget: 0.0,
+    items: [],
+  );
+
+  factory ShoppingList.fromJson(Map<String, dynamic> json) {
+    final listItems = json['items'] as List;
+
+    return empty.copyWith(
+      id: json['id'],
+      name: json['name'],
+      budget: json['budget'],
+      items: listItems.map((item) => ShoppingListItem.fromJson(item)).toList(),
+      userId: json['userId'],
+    );
+  }
+
+  // TODO: Place shopping list items here
+  final String id;
+  final String userId;
+  final String name;
+  final double budget;
+  final List<ShoppingListItem> items;
+
+  ShoppingList copyWith({
+    String? id,
+    String? name,
+    double? budget,
+    List<ShoppingListItem>? items,
+    String? userId,
+  }) {
+    return ShoppingList(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      budget: budget ?? this.budget,
+      items: items ?? this.items,
+      userId: userId ?? this.userId,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        budget,
+        items,
+        userId,
+      ];
+}
---
+++ lib/feature/shopping_list/data/model/shopping_list_item.dart
@@ -0,0 +1,62 @@
+import 'package:cooki/feature/product/data/model/output/product_output.dart';
+import 'package:cooki/feature/product/data/model/product.dart';
+import 'package:equatable/equatable.dart';
+
+class ShoppingListItem extends Equatable {
+  const ShoppingListItem({
+    required this.id,
+    required this.label,
+    required this.product,
+    required this.quantity,
+    required this.isChecked,
+  });
+
+  static const ShoppingListItem empty = ShoppingListItem(
+    id: '',
+    label: '',
+    product: Product.empty,
+    quantity: 0,
+    isChecked: false,
+  );
+
+  factory ShoppingListItem.fromJson(Map<String, dynamic> json) {
+    return empty.copyWith(
+      id: json['id'],
+      label: json['label'],
+      product: Product.fromJson(json['product']),
+      quantity: json['quantity'],
+      isChecked: json['isChecked'],
+    );
+  }
+
+  final String id;
+  final String label;
+  final Product product;
+  final int quantity;
+  final bool isChecked;
+
+  ShoppingListItem copyWith({
+    String? id,
+    String? label,
+    Product? product,
+    int? quantity,
+    bool? isChecked,
+  }) {
+    return ShoppingListItem(
+      id: id ?? this.id,
+      label: label ?? this.label,
+      product: product ?? this.product,
+      quantity: quantity ?? this.quantity,
+      isChecked: isChecked ?? this.isChecked,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        id,
+        label,
+        product,
+        quantity,
+        isChecked,
+      ];
+}
